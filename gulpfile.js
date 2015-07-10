5e30e9eead1471b059dbe2115b5a3be67303bd19
Shinnosuke Kondo <shinn.kondo@gmail.com>
2015-07-10 13:03:02 -0500

    Ready to use browser-sync (run "gulp")

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('ruby', ['C:\\home\\.gem\\ruby\\gems\\jekyll-3.0.0.pre.beta8\\bin\\jekyll', 'build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        online: false
+    });
+});
+
+gulp.task('reload', function () {
+    browserSync.reload();
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch(['*.html', '*.md', '_layouts/*.html', '_includes/*', '_posts/*', 'css/*.scss','assets/_sass/**/*.{scss,css}'], ['jekyll-rebuild']);
+    gulp.watch(['_site/*'], ['reload'])
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
